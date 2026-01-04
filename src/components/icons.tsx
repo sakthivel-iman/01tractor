9efc3a7c1855a78b6df96b0649bfda57f1556394
sakthi vel <sakthi4594@gmail.com>
2026-01-04 18:51:09 +0000

    url is data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmc

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -13,12 +13,14 @@ export const TractorIcon = (props: SVGProps<SVGSVGElement>) => (
         strokeLinejoin="round"
         {...props}
     >
-        <path d="M3 4h9l1 5h2"/>
-        <path d="M4 4v5h9"/>
-        <path d="M11 9v ilişkili"/>
-        <path d="M12 14a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2Z"/>
-        <path d="M18 18a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
-        <path d="m14.5 11-.5 1-2-1"/>
-        <path d="M18 5V3"/>
+        <path d="m10 11 11 .9a1 1 0 0 1 .8 1.1l-.665 4.158a1 1 0 0 1-.988.842H20"/>
+        <path d="M16 18h-5"/>
+        <path d="M18 5a1 1 0 0 0-1 1v5.573"/>
+        <path d="M3 4h8.129a1 1 0 0 1 .99.863L13 11.246"/>
+        <path d="M4 11V4"/>
+        <path d="M7 15h.01"/>
+        <path d="M8 10.1V4"/>
+        <circle cx="18" cy="18" r="2"/>
+        <circle cx="7" cy="15" r="5"/>
     </svg>
 );
