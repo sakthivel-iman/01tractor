2acaa1abfec50cc3c44b1b4e11417ec400988a83
sakthi vel <sakthi4594@gmail.com>
2026-01-04 17:21:16 +0000

    stop

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,7 +6,7 @@ export const TractorIcon = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth="1.5"
     strokeLinecap="round"
     strokeLinejoin="round"
     {...props}
